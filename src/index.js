e7a8bb6456662b1a363489c4b1df97ad4e4b2fd7
Igneel-98 <ghostriderattmd@gmail.com>
2024-12-29 13:38:39 +0530

    add cookie-parser and cors middleware; enhance MongoDB connection logging

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import connectDB from "./db/index.js";
 import {} from "dotenv/config";
+import express from "express";
+
+const app = express()
 
 
 connectDB()
+.then( () => {
+    app.on( "error", (error) => {
+        console.log(`ERRORS : ${error}`);
+        throw error        
+    })
+    app.listen( process.env.PORT, () => {
+        console.log(`⚙️  Server running on http://localhost:${process.env.PORT}`);
+    })
+})
+.catch( (error) => {
+    console.log(`⚠️ MONGODB CONNECTION FAILED !! : ${error}`);
+    
+})

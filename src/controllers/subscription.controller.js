0cfb2b2c6e9efdfeced66e9dd4906810f2e0efa8
Igneel-98 <ghostriderattmd@gmail.com>
2025-07-15 01:07:44 +0530

    implement toggle subscription functionality with validation and error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -5,10 +5,61 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-
+// toggle subscription
 const toggleSubscription = asyncHandler(async (req, res) => {
-    const {channelId} = req.params
-    // TODO: toggle subscription
+    const {channelIdentifier: channelId} = req.params
+    
+    // validate channelID
+    let channel;
+    if(isValidObjectId(channelIdentifier)){
+        channel = await User.findById(channelIdentifier);
+    }
+    else{
+        channel = await User.findOne({ username: channelIdentifier});
+    }
+
+    if(!channel){
+        throw new ApiError(404, "Channel Not Found");
+    }
+
+    // check if user trying to subscribe itself
+    if(req.user?._id.toString() === channel._id.toString()){
+        throw new ApiError(400, "Cannot subscribe to yourself");
+    }
+
+    // toggle subscription
+    const existingSubscription = await Subscription.findOne({
+        subscriber  : req.user._id,
+        channel : channel._id
+
+    })
+
+    if(existingSubscription){
+        // Unsubscribe
+        const unsubscribe = await Subscription.deleteOne({
+            _id : existingSubscription._id
+        })
+        if (unsubscribe.deletedCount === 0) {
+            throw new ApiError(500, "Unable to unsubscribe");
+        }
+
+
+        return res.status(200).json(
+            200, unsubscribe, "Channel Unsubscribed Successfully"
+        )
+    }
+    else{
+        // Subscribe
+        const subscribe = await Subscription.create({
+            subscriber : req.user._id,
+            channel : channel._id
+        })
+        
+        return res.status(200).json(
+            200, subscribe, "Channel Subscribed Successfully"
+        )
+
+    }
 })
 
 // controller to return subscriber list of a channel
--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -9,11 +9,19 @@ import {verifyJWT} from "../middleware/auth.middleware.js"
 const router = Router();
 router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router
-    .route("/c/:channelId")
-    .get(getSubscribedChannels)
-    .post(toggleSubscription);
+router.route("/c/:channelId")
+    .post(
+            // #swagger.tags = ['subscriptions'] 
+            toggleSubscription
+        )
+    .get(
+        // #swagger.tags = ['subscriptions'] 
+        getSubscribedChannels
+    );    
 
-router.route("/u/:subscriberId").get(getUserChannelSubscribers);
+router.route("/u/:subscriberId").get(
+    // #swagger.tags = ['subscriptions'] 
+    getUserChannelSubscribers
+);
 
 export default router
